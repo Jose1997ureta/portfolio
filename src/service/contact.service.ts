e336a67234c5068df9b1bda1e470ecba054fea13
Jose Ureta <jose.ureta@onroadts.com>
2024-08-20 22:14:57 -0500

    Se agrego la variable del backend

--- src/service/contact.service.ts
+++ src/service/contact.service.ts
@@ -10,11 +10,15 @@ export const apiSendEmail = async (element: SendEmailProps) => {
 	let rpta, error;
 
 	try {
-		rpta = await axios.post("portfolio/email", element, {
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
+		rpta = await axios.post(
+			`${process.env.NEXT_PUBLIC_API_URL}/portfolio/email`,
+			element,
+			{
+				headers: {
+					"Content-Type": "application/json",
+				},
+			}
+		);
 	} catch (error) {
 		error = "A ocurrido un problema al enviar el correo";
 	}
